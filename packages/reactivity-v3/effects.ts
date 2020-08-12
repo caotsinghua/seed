24a293d252dc40cf01f286d0f1a00d1843529ee7
caotsinghua <1577499543@qq.com>
2020-08-13 00:17:11 +0800

    effect

--- packages/reactivity-v3/effects.ts
+++ packages/reactivity-v3/effects.ts
@@ -13,7 +13,7 @@ interface ReactiveEffect<T = any> {
   _isEffect: true
   id: number
   active: boolean
-  raw: () => T
+  raw: () => T // 函数本身
   deps: Dep[]
   options: ReactiveEffectOptions
 }
@@ -62,5 +62,100 @@ export function track(target: object, type: TrackOpTypes, key: unknown) {
         key,
       })
     }
+  }
+}
+
+// === === 创建effect
+// fn是依赖收集的get
+// option是一些配置
+export function effect<T = any>(
+  fn: () => T,
+  options: ReactiveEffectOptions = {}
+) {
+  if (isEffect(fn)) {
+    fn = fn.raw
+  }
+  const effect = createReactiveEffect(fn, options)
+  if (!options.lazy) {
+    effect() // 非computed，先立刻执行一次，收集依赖
+  }
+  return effect
+}
+
+let uid = 0
+const effectStack: ReactiveEffect[] = []
+function createReactiveEffect<T = any>(
+  fn: () => T,
+  options: ReactiveEffectOptions
+): ReactiveEffect<T> {
+  const effect = function reactiveEffect(): unknown {
+    // 此处意义不明？
+    if (!effect.active) {
+      // active = false
+      // 停止侦听的时候，如果有scheduler，不执行，否则直接执行fn，即getter
+      return options.scheduler ? undefined : fn()
+    }
+    if (!effectStack.includes(effect)) {
+      cleanup(effect) // 解除所有绑定
+      try {
+        enableTraking() // 开始依赖收集
+        effectStack.push(effect) // 避免
+        activeEffect = effect
+        return fn() // 重新收集依赖
+      } finally {
+        // 依赖收集结束
+        effectStack.pop()
+        resetTraking() // 重置
+        activeEffect = effectStack[effectStack.length - 1] // activeEffect=最后一个effect
+      }
+    }
+  } as ReactiveEffect
+
+  // 赋值属性
+  effect.id = uid++
+  effect._isEffect = true
+  effect.active = true
+  effect.raw = fn // fn
+  effect.deps = []
+  effect.options = options
+  return effect
+}
+
+function cleanup(effect: ReactiveEffect) {
+  const { deps } = effect
+  if (deps && deps.length) {
+    for (let i = 0; i < deps.length; i++) {
+      deps[i].delete(effect) // 解除dep和effect的绑定
+    }
+  }
+  deps.length = 0
+}
+
+// 允许收集当前effect的依赖
+function enableTraking() {
+  trackStack.push(shouldTrack)
+  shouldTrack = true
+}
+
+// 重置收集依赖的锁
+function resetTraking() {
+  let last = trackStack.pop()
+  shouldTrack = last === undefined ? true : last
+}
+
+// ==== utils====
+function isEffect(fn: any): fn is ReactiveEffect {
+  return fn && fn._isEffect === true
+}
+
+// 停止触发更新
+function stop(effect: ReactiveEffect) {
+  if (effect.active) {
+    cleanup(effect)
+    if (effect.options.onStop) {
+      // 触发停止侦听的函数
+      effect.options.onStop()
+    }
+    effect.active = false
   }
 }
